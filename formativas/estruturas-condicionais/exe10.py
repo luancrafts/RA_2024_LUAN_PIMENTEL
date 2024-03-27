b3cc004e67f3abe9c2eb28c8e88c42e66430ce1e
luancrafts <pimentel.luan@pucpr.edu.br>
2024-03-26 21:21:45 -0300

    exercicios ate o 10

---
+++ formativas/estruturas-condicionais/exe10.py
@@ -0,0 +1,12 @@
+# Leitura
+idade=int(input("Informe a idade: "))
+
+# condicional
+if idade >= 18:
+    print("Maior de idade.")
+    if idade >= 18:
+        print("Tem idade suficiente")
+    else:
+        print("Tem idade insuficiente")
+else:
+    print("Menor de idade.")

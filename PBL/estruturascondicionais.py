85bf54ec82a0744390892d2691d9bdb2fea93531
luancrafts <pimentel.luan@pucpr.edu.br>
2024-04-01 15:05:22 -0300

    incluido a parcela a ser deduzida no PBL 1

--- PBL/estruturascondicionais.py
+++ PBL/estruturascondicionais.py
@@ -17,22 +17,22 @@ if salario_contribuinte >= 0 and salario_contribuinte <= 2259.20:
 
 elif salario_contribuinte >=0 and salario_contribuinte >=2259.21 and salario_contribuinte <= 2826.65:
     faixa_atual = "7,5%"
-    imposto = salario_contribuinte*0.075 #de acordo com a tabela
+    imposto = (salario_contribuinte*0.075) - 169.44 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 
 elif salario_contribuinte >= 2826.66 and salario_contribuinte <= 3751.05:
     faixa_atual = "15%"
-    imposto = salario_contribuinte*0.15
+    imposto = (salario_contribuinte*0.15) - 381.44 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 
 elif salario_contribuinte >= 3751.06 and salario_contribuinte <= 4664.68:
     faixa_atual = "22,5%"
-    imposto = salario_contribuinte*0.225
+    imposto = (salario_contribuinte*0.225) - 662.77 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 
 elif salario_contribuinte > 4664.68:
     faixa_atual = "27,5%"
-    imposto = salario_contribuinte*0.275
+    imposto = (salario_contribuinte*0.275) - 896 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 else:
     print("O valor inserido não é valido.")

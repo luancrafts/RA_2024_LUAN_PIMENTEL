833e76a4541ca12b16ac9cd1a3ab02f3a11d0074
luancrafts <pimentel.luan@pucpr.edu.br>
2024-04-02 20:42:28 -0300

    Correção de dedução e nova atividade de loop

--- PBL/estruturascondicionais.py
+++ PBL/estruturascondicionais.py
@@ -6,6 +6,9 @@ LUAN FELIX PIMENTEL
 ESTE ALGORITMO REALIZA A LEITURA DE SALÁRIO DE UM CONTRIBUINTE E COM BASE EM UMA TABELA ESTABELECIDA
 DE VALORES, DETERMINA A FAIXA QUE O CONTRIBUINTE SE ENCONTRA E QUAL VALOR SERÁ DESCONTADO
 EM SEU CONTRACHEQUE.
+
+Este código está disponível no repositorio: https://github.com/luancrafts/RA_2024_LUAN_PIMENTEL/
+
 """
 
 salario_contribuinte=float(input("Informe o valor de seu salário: "))
@@ -17,22 +20,22 @@ if salario_contribuinte >= 0 and salario_contribuinte <= 2259.20:
 
 elif salario_contribuinte >=0 and salario_contribuinte >=2259.21 and salario_contribuinte <= 2826.65:
     faixa_atual = "7,5%"
-    imposto = (salario_contribuinte*0.075) - 169.44 #de acordo com a tabela de dedução
+    imposto = (salario_contribuinte*0.075) + 169.44 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 
 elif salario_contribuinte >= 2826.66 and salario_contribuinte <= 3751.05:
     faixa_atual = "15%"
-    imposto = (salario_contribuinte*0.15) - 381.44 #de acordo com a tabela de dedução
+    imposto = (salario_contribuinte*0.15) + 381.44 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 
 elif salario_contribuinte >= 3751.06 and salario_contribuinte <= 4664.68:
     faixa_atual = "22,5%"
-    imposto = (salario_contribuinte*0.225) - 662.77 #de acordo com a tabela de dedução
+    imposto = (salario_contribuinte*0.225) + 662.77 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 
 elif salario_contribuinte > 4664.68:
     faixa_atual = "27,5%"
-    imposto = (salario_contribuinte*0.275) - 896 #de acordo com a tabela de dedução
+    imposto = (salario_contribuinte*0.275) + 896 #de acordo com a tabela de dedução
     print("A sua faixa de renda atual é de: ", faixa_atual, "e o valor a ser descontado em seu contracheque é de: R$ ", imposto)
 else:
     print("O valor inserido não é valido.")
---
+++ repeticao/mediadasmedias.py
@@ -0,0 +1,30 @@
+"""
+incremento seria: 
+- ter uma variavel a mais para acumular as medias
+- Adicionar esta variavel no loop
+- imprimir a media dos 50 alunos fora e ao fim do loop
+"""
+
+con = 0 #Inicialize o contador de alunos
+acumulador = 0
+
+while(con < 50):
+    n1 = float(input("Entre com a primeira nota: "))
+    n2 = float(input("Entre com a segunda nota: "))
+    n3 = float(input("Entre com a terceira nota: "))
+    n4 = float(input("Entre com a quarta nota: "))
+
+    #Calcula a média anual das notas
+    MA = (n1 + n2 + n3 +n4)/4
+    acumulador = MA
+    #Imprime a média
+    print("Média anual: ", MA)
+
+    #Verifica se o aluno foi aprovado com base na média
+    if(MA>= 7):
+        print("Aluno aprovado. Parabéns")
+    else:
+        print("Aluno Reprovado")
+        con+= 1 #Incrementa
+acumulador = acumulador/50
+print("A média das médias é:", acumulador)

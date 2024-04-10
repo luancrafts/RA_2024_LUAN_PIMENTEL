d4461941afdbecb046b8bf43f1d729980a4960f7
luancrafts <pimentel.luan@pucpr.edu.br>
2024-04-10 19:11:12 -0300

    simulado

--- formativas/matematica/atividade01.py
+++ formativas/matematica/atividade01.py
@@ -4,3 +4,17 @@ LUAN JOSE, DANTON TALLES, LUCAS MORO
 01.04.2024
 """
 
+cores = ["vermelho", "azul", "amarelo"]
+for cor in cores:
+    print("Estou pintando de ", cor)
+
+for numero in range (1, 10):
+    print(numero)
+
+numero = 1
+while numero <=5:
+    print(numero)
+    numero+= 1
+
+# range(10,0,-1)
+# gerará os números de 10 a 1 (mas sem incluir 0), em ordem decrescente, com um passo de -1.
---
+++ simulado/respostas.py
@@ -0,0 +1,55 @@
+"""
+PONTIFICIA UNIVERSIDADE CATOLICA DO PARANÁ
+LUAN FELIX PIMENTEL
+09.04.2024
+
+1) Algoritmos pode ser definido como um conjunto finito de passos que com base em inputs (entradas) realiza uma série de
+instruções definidas e apresenta como resultado outputs (saídas).
+
+2) Os quatro tipos primitivos estudados são inteiro, real, caractere e lógico
+
+3) Variável é um tipo de dado que pode variar conforme o tempo de vida do algoritmo, ou seja, ele pode mudar. 
+Já constante representa um tipo de dado que será mantido íntegro por todo esse tempo de vida do algoritmo, ou seja, não muda.
+
+4) As operações CONDICIONAIS... IF
+
+5) Um LAÇO é uma estrutura que permite.. com base em uma regra ou um INTERVALO... WHILE e FOR
+
+6) 
+tempofoco = int(input("Qual o tempo de foco que gostaria?: "))
+tempodepausa = int(input("Qual o tempo de pausa que gostaria?: "))
+
+
+7) idade = int(input("Qual a sua idade? :"))
+if (idade>=0 and idade<=12):
+    print("Com base na sua idade você é uma criança")
+elif (idade>12 and idade <= 18):
+    print("Com base na sua idade você é um adolescente")
+elif (idade>18):
+    print("Com base na sua idade você é um adulto")
+else:
+    print("A idade informada é inválida")
+
+    
+8) 
+numero = int(input("Escreva um numero: ")
+#if(numero % 2 == 0):
+    print("O número", numero,"é positivo")
+elif (numero <0):
+    print("O número", numero, "é negativo")
+else:
+        print("O número é zero")
+
+9) 
+departamento= input("Informe o nome do departmamento: ")
+responsavel = input("Qual o nome do responsável?: ")
+print("O departamento de", departamento,"é liderardo por", responsavel)
+
+10)
+numero = int(input("Informe o nunero para calcular a tabuada: "))
+con=1
+while (con<=10):
+    #calculo = con*numero
+    print(numero, "x", con, "=", con*numero)
+    con+=1
+"""

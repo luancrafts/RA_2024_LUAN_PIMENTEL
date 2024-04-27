07d05dc6ff136839071f982238f58b7a38fc5424
luancrafts <pimentel.luan@pucpr.edu.br>
2024-04-26 22:25:12 -0300

    Novas alterações em vetores

--- formativas/exercicios/exe1.py
+++ formativas/exercicios/exe1.py
@@ -1,4 +1,9 @@
 index = 0
 vetor = [1,2,3,4,5]
 index = int(input("Digite o numero do index que deseha consultar: "))
-print(vetor[index])
+
+if index>=0 and index <len(vetor):
+    print(vetor[index])
+    
+else:
+    print("O indice fornecido está fora dos limites do vetor.")
--- formativas/exercicios/vetores/exe2.py
+++ formativas/exercicios/vetores/exe2.py
@@ -3,10 +3,12 @@ opcao = 1
 
 while opcao != 0:
     opcao = int(input("Digite 0 para sair. 1 Para adicionar numero e 2 para remover: "))
+
     if opcao == 1:
         numero = int(input("Digite o numero desejado: "))
         vetor.append(numero)
         vetor.sort()
+
     elif opcao == 2:
         numero = int(input("Digite o numero que deseha remover: "))
         vetor.remove(numero)
